58a5056c19336c1a486f086b913e925f01a0ff15
Alexander Chavez <freddyalexander2.chavez@gmail.com>
2023-08-15 15:42:20 -0600

    Meta-Tags-og

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -12,6 +12,26 @@ export const Layout: FC<Props> = ({ children, title }) => {
     <div className='relative overflow-x-hidden'>
       <Head>
         <title>{title}</title>
+        <meta property='og:title' content='Logiciel Applab' />
+        <meta
+          property='og:description'
+          content='Somos una empresa especializada en el desarrollo de aplicaciones móviles, sitios web y sistemas de software hechos según tus necesidades. Con nuestro equipo altamente calificado te garantizamos la mejor calidad y experiencia para tu negocio.'
+        />
+        <meta property='og:image' content='https://www.logicielapplab.com/images/logo.png' />
+
+        <title>Logiciel Applab - Desarrollo de Aplicaciones Móviles y Sitios Web</title>
+        <meta
+          name='description'
+          content='Somos una empresa especializada en el desarrollo de aplicaciones móviles, sitios web y sistemas de software hechos según tus necesidades. Con nuestro equipo altamente calificado te garantizamos la mejor calidad y experiencia para tu negocio.'
+        />
+        <meta
+          name='keywords'
+          content='desarrollo de aplicaciones móviles, desarrollo web, sistemas de software, empresa de desarrollo de software, Logiciel Applab'
+        />
+        <meta name='author' content='Logiciel Applab' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+
+        <link rel='canonical' href='https://www.logicielapplab.com/' />
       </Head>
       <Navbar />
       <main>{children}</main>
@@ -19,6 +39,4 @@ export const Layout: FC<Props> = ({ children, title }) => {
       <Footer />
     </div>
   );
-
-  /* Navbar */
 };
